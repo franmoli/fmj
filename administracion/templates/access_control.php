7cdecdcb5af2c6fd9a123f0b4a412d7574acd679
franmoli <fran.moli222@gmail.com>
2024-03-10 17:06:29 -0300

    access control con admin level

--- administracion/templates/access_control.php
+++ administracion/templates/access_control.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-if ($_SESSION['logueado'] == 0) echo '<meta http-equiv="refresh" content="0; url=login.php">';
+if ($_SESSION['logueado'] == 0) header("Location: login.php");;
 
 if ($_SESSION['admin_level'] > 1) {
     $nombre_archivo = basename($_SERVER['REQUEST_URI']);
     $paginas_permitidas = array("index.php", "crear-competidor.php", "administrar-competidores.php");
     if (!in_array($nombre_archivo, $paginas_permitidas)) {
-        echo '<meta http-equiv="refresh" content="0; url=login.php">';
+        header("Location: login.php");
     }
 }
