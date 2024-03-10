16dbb228679c2b17649d5f11ad134715a38b086a
franmoli <fran.moli222@gmail.com>
2024-03-10 16:49:21 -0300

    logout

--- administracion/logout.php
+++ administracion/logout.php
@@ -5,14 +5,14 @@ try {
     session_start();
     session_unset();
     session_destroy();
-    echo json_encode(['success' => true]);
+    // echo json_encode(['success' => true]);
     // Redireccionar a otra página después de 5 segundos
     header("refresh:5;url=../index.php");
 
     // También puedes usar la función header directamente
     // header("Location: otra_pagina.php");
 
-    echo "Serás redireccionado a otra página en 5 segundos. Si no, puedes hacer clic <a href='../index.php'>aquí</a>.";
+    echo "Se cerró correctamente la sesion. Serás redireccionado a otra página en 5 segundos. Si no, puedes hacer clic <a href='../index.php'>aquí</a>.";
 } catch (Exception $e) {
     echo json_encode(['success' => false]);
 }
