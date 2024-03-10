735fac1ac8aa53c33b9881bbc81eb9d3fd432b99
franmoli <fran.moli222@gmail.com>
2024-03-10 17:08:05 -0300

    access control con admin level

--- administracion/templates/sidebar.php
+++ administracion/templates/sidebar.php
@@ -14,25 +14,25 @@
             </div>
 
             <br>
+            <?php if ($_SESSION['admin_level'] <= 1) : ?>
+                <div class="categoria text-center">
+                    <h2 class="light-overlay"><i class="fas fa-trophy"></i>Torneos</h2>
+                    <a href="crear-torneo.php"><i class="fas fa-plus-circle"></i>Nuevo</a>
+                    <br>
+                    <a href="administrar-torneos.php"><i class="fas fa-edit"></i>Administrar</a>
+                </div>
 
-            <div class="categoria text-center">
-                <h2 class="light-overlay"><i class="fas fa-trophy"></i>Torneos</h2>
-                <a href="crear-torneo.php"><i class="fas fa-plus-circle"></i>Nuevo</a>
                 <br>
-                <a href="administrar-torneos.php"><i class="fas fa-edit"></i>Administrar</a>
-            </div>
 
-            <br>
+                <div class="categoria text-center">
+                    <h2 class="light-overlay"><i class="fas fa-graduation-cap"></i>Profesores</h2>
+                    <a href="crear-profesor.php"><i class="fas fa-plus-circle"></i>Nuevo</a>
+                    <br>
+                    <a href="administrar-profesores.php"><i class="fas fa-edit"></i>Administrar</a>
+                </div>
 
-            <div class="categoria text-center">
-                <h2 class="light-overlay"><i class="fas fa-graduation-cap"></i>Profesores</h2>
-                <a href="crear-profesor.php"><i class="fas fa-plus-circle"></i>Nuevo</a>
                 <br>
-                <a href="administrar-profesores.php"><i class="fas fa-edit"></i>Administrar</a>
-            </div>
-
-            <br>
-
+            <?php endif; ?>
             <div class="categoria text-center">
                 <h2 class="light-overlay"><a href="./logout.php"><i class="fas fa-arrow-circle-left"></i>Volver al sitio</a></h2>
             </div>
