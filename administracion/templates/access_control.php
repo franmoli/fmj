ea8f62901603ad9cdf4730579574324d449fa191
franmoli <fran.moli222@gmail.com>
2024-03-10 17:36:08 -0300

    editar competidor profe

--- administracion/templates/access_control.php
+++ administracion/templates/access_control.php
@@ -5,7 +5,8 @@ if ($_SESSION['logueado'] == 0) header("Location: login.php");;
 if ($_SESSION['admin_level'] > 1) {
     $nombre_archivo = basename($_SERVER['REQUEST_URI']);
     $paginas_permitidas = array("index.php", "crear-competidor.php", "administrar-competidores.php", "editar-competidor.php");
+    echo $nombre_archivo;
     if (!in_array($nombre_archivo, $paginas_permitidas)) {
-        header("Location: login.php");
+        // header("Location: login.php");
     }
 }
